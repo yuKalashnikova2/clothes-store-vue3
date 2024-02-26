b1417e65bfb1384fffc6959aae4858160157c5a7
yuKalasnikova2 <pmalian@list.ru>
2024-02-26 15:51:15 +0300

    add swiper vue library for banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,34 +1,63 @@
+<script setup>
+import { Scrollbar, Autoplay } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+import 'swiper/scss'
+import 'swiper/scss/scrollbar'
+</script>
 <template>
-  <div class="banner">
-    <div class="banner__text">
-      <h5 class="banner__subtitle">T-shirt / Tops</h5>
-      <h1 class="banner__title">Summer Value Pack</h1>
-      <h4 class="banner__subtitle">cool / colorful / comfy</h4>
-      <router-link to="/shop">
-        <button class="banner__button">Shop Now</button>
-      </router-link>
-    </div>
-    <div class="for"></div>
-  </div>
+  <!-- :autoplay="{ delay: 5000 }" -->
+  <Swiper
+    :slides-per-view="1"
+    :touch="true"
+    :space-between="50"
+    :modules="[Scrollbar, Autoplay]"
+    :scrollbar="{ draggable: true }"
+    effect="scrollbar"
+  >
+    <SwiperSlide>
+      <div class="banner">
+        <div class="banner__text">
+          <h5 class="banner__subtitle">T-shirt / Tops</h5>
+          <h1 class="banner__title">Summer Value Pack</h1>
+          <h4 class="banner__subtitle">cool / colorful / comfy</h4>
+          ool / Colorful / Comfy Shop Now
+
+          <router-link to="/shop">
+            <button class="banner__button">Shop Now</button>
+          </router-link>
+        </div>
+        <div class="for"></div>
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div class="banner">
+        <div class="banner__text"></div>
+        <div class="for"></div>
+      </div>
+    </SwiperSlide>
+  </Swiper>
 </template>
 
 <style lang="scss" scoped>
 .banner {
-  background: url('/banner.png') top center no-repeat;
+  background-image: url('/banner.png');
+  background-position: center;
   background-size: cover;
   color: #fff;
   display: grid;
   grid-template-columns: 1fr 1fr;
   height: 716px;
+  width: 100%;
   @media (max-width: 992px) {
     display: block;
-    height: auto;
+    height: 700px;
   }
   &__text {
     display: flex;
     flex-direction: column;
-    gap: 40px;
-    padding: 130px 0 130px 193px;
+    gap: 30px;
+    padding: 88px 0 130px 193px;
     @media (max-width: 992px) {
       padding: 30px 10px 30px 50px;
     }
@@ -70,6 +99,12 @@
 .for {
   @media (max-width: 767px) {
     display: none;
+  }
+}
+.swiper {
+  background-color: #fff;
+  & img {
+    width: 100%;
   }
 }
 </style>
