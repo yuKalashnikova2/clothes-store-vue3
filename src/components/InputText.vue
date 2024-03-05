b17ac892a8d743bfd079d80ab2675b07682215c0
yuKalasnikova2 <pmalian@list.ru>
2024-03-05 10:48:52 +0300

    feat(component): InputText; refactor SignIn

---
+++ src/components/InputText.vue
@@ -0,0 +1,65 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text',
+  },
+  placeholder: {
+    type: String,
+    default: 'Placeholder',
+  },
+  name: {
+    type: String,
+    default: 'Name',
+  },
+  label: {
+    type: String,
+    default: 'Label',
+  },
+  value: {
+    type: String,
+    required: false,
+  },
+})
+const emits = defineEmits(['update:value'])
+</script>
+<template>
+  <div class="input">
+    <label class="input__label" :for="name"
+      >{{ label }}
+      <slot></slot>
+    </label>
+
+    <input
+      class="input__text"
+      :type="type"
+      :placeholder="placeholder"
+      :name="name"
+      :value="value"
+      @input="$emit('update:value', $event.target.value)"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  &__label {
+    font-weight: 400;
+    font-size: 18px;
+    letter-spacing: 0.02em;
+    color: #3c4242;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__text {
+    border: 1px solid #3c4242;
+    border-radius: 8px;
+    padding-left: 15px;
+    padding: 17px 15px;
+  }
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,9 +1,22 @@
 <script setup>
-import HeaderSign from '../components/HeaderSign.vue'
+import { ref } from 'vue'
+import Button from '../components/Button.vue'
+import InputText from '../components/InputText.vue'
+
+const email = ref('')
+const password = ref('')
+const isPasswordVisible = ref(false)
+
+const togglePasswordVisibility = () => {
+  isPasswordVisible.value = !isPasswordVisible.value
+}
+
+const changeTypePassword = () => {
+  return isPasswordVisible ? text : password
+}
 </script>
 
 <template>
-  <HeaderSign />
   <div class="sign-in">
     <div class="sign-in__image">
       <img src="/signin.png" alt="pic" />
@@ -11,6 +24,63 @@ import HeaderSign from '../components/HeaderSign.vue'
 
     <div class="sign-in__form">
       <h2 class="sign-in__form__title">Sign In Page</h2>
+
+      <form class="sign-in__form__submit" @submit.prevent>
+        <div class="sign-in__form__submit__buttons">
+          <Button label="Continue With Google" color="secondary" />
+          <Button label="Continue With Twitter" color="secondary" />
+        </div>
+        <div class="sign-in__form__submit__divider">
+          <div class="divider"></div>
+          <span class="sign-in__form__submit__divider__text">or</span>
+          <div class="divider"></div>
+        </div>
+
+        <div class="sign-in__form__submit__inputs">
+          <InputText
+            type="email"
+            placeholder="Email"
+            label="User name or email address"
+            v-model:value.trim="email"
+            name="email"
+          />
+          <div>
+            <InputText
+              :type="isPasswordVisible ? 'text' : 'password'"
+              placeholder="Password"
+              label="Password"
+              v-model:value.trim="password"
+              name="password"
+            >
+              <div
+                class="sign-in__form__submit__inputs__hide"
+                @click="togglePasswordVisibility"
+              >
+                <img v-if="isPasswordVisible" src="/hide-open.svg" alt="" />
+                <img v-else src="/hide.svg" alt="hide" />
+                <span v-if="isPasswordVisible">Hide</span>
+                <span v-else>Show</span>
+              </div>
+            </InputText>
+            <router-link
+              class="sign-in__form__submit__inputs__forgot"
+              to="/reset-password"
+            >
+              Forget your password
+            </router-link>
+          </div>
+
+          <div class="sign-in__form__submit__inputs__sign">
+            <router-link to="/">
+              <Button label="Sign In" />
+            </router-link>
+            <span>
+              Don’t have an account?
+              <router-link to="/signup">Sign up </router-link>
+            </span>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -18,13 +88,14 @@ import HeaderSign from '../components/HeaderSign.vue'
 <style lang="scss" scoped>
 .sign-in {
   display: flex;
+  gap: 70px;
   @media (max-width: 992px) {
     flex-direction: column;
   }
   &__image {
     img {
-      @media (max-width: 992px) {
-        height: 100px;
+      @media (max-width: 767px) {
+        display: none;
       }
     }
     @media (max-width: 992px) {
@@ -32,7 +103,13 @@ import HeaderSign from '../components/HeaderSign.vue'
     }
   }
   &__form {
-    padding: 60px 100px 0 77px;
+    padding-top: 60px;
+    flex-grow: 1;
+    @media (max-width: 992px) {
+      order: 1;
+      padding-top: 30px;
+      padding-left: 30px;
+    }
     &__title {
       font-family: var(--second-family);
       font-weight: 600;
@@ -40,6 +117,74 @@ import HeaderSign from '../components/HeaderSign.vue'
       letter-spacing: 0.02em;
       color: #333;
     }
+    &__submit {
+      padding-right: 100px;
+      &__buttons {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        width: 100%;
+        margin: 50px 0;
+      }
+      &__divider {
+        display: flex;
+        gap: 24px;
+        align-items: center;
+        &__text {
+          font-weight: 400;
+          font-size: 18px;
+          color: #666;
+          text-transform: uppercase;
+        }
+      }
+      &__inputs {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+        margin-top: 50px;
+        &__sign {
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
+          font-weight: 400;
+          font-size: 16px;
+          color: #3c4242;
+          a {
+            display: inline-block;
+            text-decoration: underline;
+          }
+        }
+        &__hide {
+          display: flex;
+          gap: 15px;
+          font-weight: 400;
+          font-size: 18px;
+          text-align: right;
+          color: #807d7e;
+          cursor: pointer;
+        }
+        &__forgot {
+          font-weight: 400;
+          font-size: 16px;
+          text-decoration: underline;
+          text-decoration-skip-ink: none;
+          text-align: right;
+          color: #3c4242;
+          text-decoration: underline;
+          display: block;
+          text-align: end;
+          margin-top: 10px;
+          @media (max-width: 992px) {
+            text-align: start;
+          }
+        }
+      }
+    }
   }
+}
+.divider {
+  height: 2px;
+  background: rgba(102, 102, 102, 0.25);
+  width: 100%;
 }
 </style>
