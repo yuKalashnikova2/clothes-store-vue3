03719ed30b4b59fd8b4350363f9184dd8ce5c59f
yuKalasnikova2 <pmalian@list.ru>
2024-04-18 16:49:16 +0300

    refactoring MainPage

--- src/components/CardCategories.vue
+++ src/components/CardCategories.vue
@@ -1,85 +1,86 @@
 <script setup>
-import {useProductsStore} from '../stores/index.js'
-const store = useProductsStore()
-
-  const props = defineProps({
-  productsMen: {
-    type: Array,
-    required: true
-  }
+const props = defineProps({
+    products: {
+        type: Array,
+        required: true,
+    },
+    isMale: {
+        type: Boolean,
+        required: false,
+    },
 })
-
 </script>
 
 <template>
-  <div class="card__product" v-for="product in store.productsMen">
-    <div class="card__product__image">
-      <img :src="product.url" :alt="product.name" />
-    </div>
+    <div class="card__product" v-for="product in products">
+        <div class="card__product__image">
+            <img :src="product.link_img[0]" :alt="product.name" />
+        </div>
 
-    <div class="card__product__info">
-      <div class="card__product__text">
-        <h5 class="card__product__title">{{ product.name }}</h5>
-        <h3 class="card__product__subtitle">Explore Now!</h3>
-      </div>
-      <router-link to="/next">
-        <img class="card__product__arrow" src="/arrow.svg" alt="next" />
-      </router-link>
+        <div class="card__product__info">
+            <div class="card__product__text">
+                <h5 class="card__product__title">{{ product.name }}</h5>
+                <h3 class="card__product__subtitle">Explore Now!</h3>
+            </div>
+            <router-link :to="isMale ? '/shopmen' : '/shopwomen'">
+                <img class="card__product__arrow" src="/arrow.svg" alt="next" />
+            </router-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
 .card__product {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  border-radius: 10px;
-
-  &__info {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  &__image {
-    width: 100%;
-    object-fit: cover;
-    overflow: hidden;
+    flex-direction: column;
+    gap: 10px;
     border-radius: 10px;
-    & img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-  &__title {
-    font-weight: 700;
-    font-size: 17px;
 
-    letter-spacing: -0.04em;
-    color: #2a2f2f;
-    padding-bottom: 3px;
-  }
-  &__subtitle {
-    font-weight: 500;
-    font-size: 13px;
-
-    letter-spacing: -0.04em;
-    color: #7f7f7f;
-  }
-  &__arrow {
-    width: 20px;
-    height: 14px;
-    cursor: pointer;
-
-    img {
-      width: 100%;
+    &__info {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
-    &:hover {
-      transform: translateX(0.25rem);
-      transition-duration: 150ms;
-      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    &__image {
+        width: 100%;
+        object-fit: cover;
+        overflow: hidden;
+        border-radius: 10px;
+        height: 270px;
+        & img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
     }
-  }
+    &__title {
+        font-weight: 700;
+        font-size: 17px;
+
+        letter-spacing: -0.04em;
+        color: #2a2f2f;
+        padding-bottom: 3px;
+    }
+    &__subtitle {
+        font-weight: 500;
+        font-size: 13px;
+
+        letter-spacing: -0.04em;
+        color: #7f7f7f;
+    }
+    &__arrow {
+        width: 20px;
+        height: 14px;
+        cursor: pointer;
+
+        img {
+            width: 100%;
+        }
+        &:hover {
+            transform: translateX(0.25rem);
+            transition-duration: 150ms;
+            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        }
+    }
 }
 </style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,202 +1,206 @@
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Pagination } from 'swiper/modules'
-
+import { useFirebaseStore } from '../stores/getDB.js'
 import Button from '../components/Button.vue'
 import Banner from '../components/Banner.vue'
 import WidgetCardList from '../components/WidgetCardList.vue'
 import CardCategories from '../components/CardCategories.vue'
 import Title from '../components/Title.vue'
-import { useProductsStore } from '../stores/index.js'
+
 import TopBrandsDeal from '../components/TopBrandsDeal.vue'
 import FeedbacCard from '../components/FeedbacCard.vue'
 import 'swiper/css'
 import 'swiper/css/pagination'
 
-const store = useProductsStore()
+const db = useFirebaseStore()
 </script>
 
 <template>
-  <div>
-    <Banner />
+    <div>
+        <Banner />
 
-    <div class="content">
-      <WidgetCardList />
+        <div class="content">
+            <WidgetCardList />
 
-      <div class="content__info">
-        <div class="content__info__text">
-          <div>
-            <h2 class="content__info__title">
-              WE MADE YOUR EVERYDAY FASHION BETTER!
-            </h2>
-            <span class="content__info__subtitle"
-              >In our journey to improve everyday fashion, euphoria presents
-              EVERYDAY wear range - Comfortable & Affordable fashion 24/7</span
-            >
-            <router-link class="content__info__button" to="/shop">
-              <Button label="Shop Now" size="medium" />
-            </router-link>
-          </div>
+            <div class="content__info">
+                <div class="content__info__text">
+                    <div>
+                        <h2 class="content__info__title">
+                            WE MADE YOUR EVERYDAY FASHION BETTER!
+                        </h2>
+                        <span class="content__info__subtitle"
+                            >In our journey to improve everyday fashion,
+                            euphoria presents EVERYDAY wear range - Comfortable
+                            & Affordable fashion 24/7</span
+                        >
+                        <router-link class="content__info__button" to="/shop">
+                            <Button label="Shop Now" size="medium" />
+                        </router-link>
+                    </div>
+                </div>
+
+                <div class="content__info__image">
+                    <img src="/content/content-4.png" alt="pic" />
+                </div>
+            </div>
+
+            <div class="content__decor">
+                <div>
+                    <Title title="Categories For Men" decor />
+                    <div class="card__list">
+                        <CardCategories :products="db.men" :isMale="true" />
+                    </div>
+                </div>
+
+                <div>
+                    <Title title="Categories For Women" decor="" />
+                    <div class="card__list">
+                        <CardCategories :products="db.women.slice(0, 4)" />
+                    </div>
+                </div>
+            </div>
+
+            <TopBrandsDeal />
+
+            <div class="content_margin_135">
+                <Title title="Feedback" decor />
+                <swiper :modules="[Pagination]" :pagination="true">
+                    <SwiperSlide class="content__feedback">
+                        <FeedbacCard />
+                        <FeedbacCard />
+                        <FeedbacCard />
+                    </SwiperSlide>
+
+                    <SwiperSlide class="content__feedback">
+                        <FeedbacCard />
+                        <FeedbacCard />
+                        <FeedbacCard />
+                    </SwiperSlide>
+                    <SwiperSlide class="content__feedback">
+                        <FeedbacCard />
+                        <FeedbacCard />
+                        <FeedbacCard />
+                    </SwiperSlide>
+                </swiper>
+            </div>
         </div>
-
-        <div class="content__info__image">
-          <img src="/content/content-4.png" alt="pic" />
-        </div>
-      </div>
-
-      <div class="content__decor">
-        <div>
-          <Title title="Categories For Men" decor />
-          <div class="card__list">
-            <CardCategories :productsMen="store.productsMen" />
-          </div>
-        </div>
-
-        <div>
-          <Title title="Categories For Women" decor="" />
-        </div>
-      </div>
-
-      <TopBrandsDeal />
-
-      <div class="content_margin_135">
-        <Title title="Feedback" decor />
-        <swiper :modules="[Pagination]" :pagination="true">
-          <SwiperSlide class="content__feedback">
-            <FeedbacCard />
-            <FeedbacCard />
-            <FeedbacCard />
-          </SwiperSlide>
-
-          <SwiperSlide class="content__feedback">
-            <FeedbacCard />
-            <FeedbacCard />
-            <FeedbacCard />
-          </SwiperSlide>
-          <SwiperSlide class="content__feedback">
-            <FeedbacCard />
-            <FeedbacCard />
-            <FeedbacCard />
-          </SwiperSlide>
-        </swiper>
-      </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss">
 .content {
-  width: 100%;
-  padding: 80px 100px;
+    width: 100%;
+    padding: 80px 100px;
 
-  @media (max-width: 992px) {
-    padding: 30px 20px;
-  }
-  &__info {
-    display: flex;
-    color: #fff;
-    margin-top: 72px;
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-    border-radius: 12px;
-    overflow: hidden;
     @media (max-width: 992px) {
-      flex-direction: column;
+        padding: 30px 20px;
     }
-    &__text {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-image: url('/content/content-3.png');
-      background-size: cover;
-      background-position: center;
-      padding: 0 74px;
-      @media (max-width: 992px) {
-        padding: 40px 30px;
-      }
+    &__info {
+        display: flex;
+        color: #fff;
+        margin-top: 72px;
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+        border-radius: 12px;
+        overflow: hidden;
+        @media (max-width: 992px) {
+            flex-direction: column;
+        }
+        &__text {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-image: url('/content/content-3.png');
+            background-size: cover;
+            background-position: center;
+            padding: 0 74px;
+            @media (max-width: 992px) {
+                padding: 40px 30px;
+            }
+        }
+        &__title {
+            font-family: var(--second-family);
+            font-weight: 800;
+            font-size: 34px;
+            line-height: 147%;
+            letter-spacing: 0.01em;
+            padding-bottom: 30px;
+            @media (max-width: 992px) {
+                font-size: 24px;
+            }
+        }
+        &__subtitle {
+            display: block;
+            font-family: var(--font-family);
+            font-weight: 300;
+            font-size: 20px;
+            letter-spacing: 0.05em;
+        }
+        &__image {
+            flex-shrink: 0;
+            img {
+                display: block;
+                width: 100%;
+                height: 100%;
+            }
+        }
+        &__button {
+            display: block;
+            padding-top: 50px;
+        }
     }
-    &__title {
-      font-family: var(--second-family);
-      font-weight: 800;
-      font-size: 34px;
-      line-height: 147%;
-      letter-spacing: 0.01em;
-      padding-bottom: 30px;
-      @media (max-width: 992px) {
-        font-size: 24px;
-      }
+    &__decor {
+        margin-top: 80px;
+        @media (max-width: 992px) {
+            margin-top: 50px;
+        }
+        & > * {
+            margin-bottom: 103px;
+            @media (max-width: 992px) {
+                margin-bottom: 50px;
+            }
+        }
     }
-    &__subtitle {
-      display: block;
-      font-family: var(--font-family);
-      font-weight: 300;
-      font-size: 20px;
-      letter-spacing: 0.05em;
+    &_margin_135 {
+        margin-top: 135px;
+        @media (max-width: 992px) {
+            margin-top: 35px;
+        }
     }
-    &__image {
-      flex-shrink: 0;
-      img {
-        display: block;
-        width: 100%;
-        height: 100%;
-      }
+    &__feedback {
+        margin-top: 70px;
+        display: flex;
+        gap: 23px;
+        @media (max-width: 992px) {
+            flex-direction: column;
+            margin-top: 35px;
+        }
     }
-    &__button {
-      display: block;
-      padding-top: 50px;
-    }
-  }
-  &__decor {
-    margin-top: 80px;
-    @media (max-width: 992px) {
-      margin-top: 50px;
-    }
-    & > * {
-      margin-bottom: 103px;
-      @media (max-width: 992px) {
-        margin-bottom: 50px;
-      }
-    }
-  }
-  &_margin_135 {
-    margin-top: 135px;
-    @media (max-width: 992px) {
-      margin-top: 35px;
-    }
-  }
-  &__feedback {
-    margin-top: 70px;
-    display: flex;
-    gap: 23px;
-    @media (max-width: 992px) {
-      flex-direction: column;
-      margin-top: 35px;
-    }
-  }
 }
 .card__list {
-  display: flex;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 50px;
-  margin-top: 70px;
-  @media (max-width: 992px) {
-    grid-template-columns: repeat(2, 1fr);
-    gap: 25px;
-    margin-top: 25px;
-  }
-  @media (max-width: 767px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
+    display: flex;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 50px;
+    margin-top: 70px;
+    @media (max-width: 992px) {
+        grid-template-columns: repeat(2, 1fr);
+        gap: 25px;
+        margin-top: 25px;
+    }
+    @media (max-width: 767px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 }
 
 .swiper-pagination-bullet {
-  background-color: rgba(34, 34, 34, 0.3);
-  @media (max-width: 992px) {
-    display: none;
-  }
+    background-color: rgba(34, 34, 34, 0.3);
+    @media (max-width: 992px) {
+        display: none;
+    }
 
-  &-active {
-    background-color: #3c4242;
-  }
+    &-active {
+        background-color: #3c4242;
+    }
 }
 </style>
